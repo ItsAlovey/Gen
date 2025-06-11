64449f4ab90ad794311aac94c12d58111762c631
Alovey <aloveyy@icloud.com>
2025-06-11 15:04:24 -0400

    Update 1.1
    I reorganized the file by moving the the variables to the top. I also just add some more comments explaining what each line of code does.

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 import secrets
 import datetime
 
-#Gender and Name 
+#Variables
 gender = secrets.randbelow(2) #pick between male or female name list
-if gender == 0:
+currentYear = datetime.datetime.now().year  # getting the current year
+currentMonth = datetime.datetime.now().month  # getting the current month number
+currentDay = datetime.datetime.now().day  # getting the current day number
+age = secrets.SystemRandom().randint(13, 80) # generating a random age between 13 and 80
+birthYear = currentYear - age  # calculating the birth year based on the current year and age
+monthGen = secrets.SystemRandom().randint(1, 12) # generating a random month number between 1 and 12
+
+#Gender and Name Generation
+if gender == 0: 
     nameList = "male_names.txt"
     sex = "Male" #setting the sex of the character to male 
 
@@ -21,69 +29,63 @@ with open('last_names.txt') as f: #opens file with last names
     lastName = lines[secrets.randbelow(len(lines))].strip()  #strips the the newline from the name
 
 #Age and Birth Date
-currentYear = datetime.datetime.now().year  # getting the current year
-currentMonth = datetime.datetime.now().month  # getting the current month number
-currentDay = datetime.datetime.now().day  # getting the current day number
-age = secrets.SystemRandom().randint(13, 80) # generating a random age between 13 and 80
-birthYear = currentYear - age  # calculating the birth year based on the current year and age
-monthGen = secrets.SystemRandom().randint(1, 12) # generating a random month number between 1 and 12
-
 if monthGen == 1:
-    month = "January"
-    days = 31
+    month = "January" # setting the month to January
+    days = 31 #setting the number of days in January
 elif monthGen == 2:
-    month = "February"
+    month = "February" # setting the month to February
+    # Check if the current year is a leap year to determine the number of days in February
     if datetime.datetime.now().year % 4 == 0 and (datetime.datetime.now().year % 100 != 0 or datetime.datetime.now().year % 400 == 0):
-        days = 29
+        days = 29 # If it's a leap year, February has 29 days
     else:
-        days = 28  # Assuming non-leap year for simplicity
+        days = 28  # If it's not a leap year, February has 28 days
 elif monthGen == 3:
-    month = "March"
-    days = 31
+    month = "March" # setting the month to March
+    days = 31 #setting the number of days in March
 elif monthGen == 4:
-    month = "April"
-    days = 30
+    month = "April" # setting the month to April
+    days = 30 #setting the number of days in April
 elif monthGen == 5:
-    month = "May"
-    days = 31
+    month = "May" # setting the month to May
+    days = 31 #setting the number of days in May
 elif monthGen == 6:
-    month = "June"
-    days = 30
+    month = "June" # setting the month to June
+    days = 30 #setting the number of days in June
 elif monthGen == 7:
-    month = "July"
-    days = 31
+    month = "July" # setting the month to July
+    days = 31 #setting the number of days in July
 elif monthGen == 8:
-    month = "August"
-    days = 31
+    month = "August" # setting the month to August
+    days = 31 #setting the number of days in August
 elif monthGen == 9:
-    month = "September"
-    days = 30
-elif monthGen == 10:
-    month = "October"
-    days = 31
+    month = "September" # setting the month to September
+    days = 30 #setting the number of days in September
+elif monthGen == 10: 
+    month = "October" # setting the month to October
+    days = 31 #setting the number of days in October
 elif monthGen == 11:
-    month = "November"
-    days = 30
+    month = "November" # setting the month to November
+    days = 30 #setting the number of days in November
 elif monthGen == 12:
-    month = "December"
-    days = 31
+    month = "December" # setting the month to December
+    days = 31 #setting the number of days in December
 else:
     print("Invalid month number")
 # This code generates a random month number and prints the corresponding month name.
 
 day = secrets.SystemRandom().randint(1, days)  # generating a random day based on the month
 
-if monthGen > currentMonth:
+if monthGen > currentMonth: #
     age -= 1  # If the generated month is greater than the current month, subtract one from the age
 elif monthGen == currentMonth and day > currentDay:
-    age -= 1
+    age -= 1 # If the generated month is the same as the current month but the day is greater, subtract one from the age
 
 #From
-with open('countries.txt') as f: #opening the country file
-    lines = f.readlines() #searching the country file
-country = lines[secrets.randbelow(len(lines))].strip() #then stripinng the newline character from the file
+with open('countries.txt') as f: #opens file with countries
+    lines = f.readlines() #picking randomly from the list of countries
+country = lines[secrets.randbelow(len(lines))].strip() # #picking a random country from the list of countries
 
-
+# States and Provinces 
 usStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
            "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
            "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
@@ -109,23 +111,23 @@ ieStates = ["Carlow", "Cavan", "Clare", "Cork", "Donegal", "Dublin", "Galway", "
               "Wexford", "Wicklow"]
 
 if country == "United States":
-   state = usStates[secrets.randbelow(len(usStates))]
+   state = usStates[secrets.randbelow(len(usStates))] # picking a random state from the US states list
 elif country == "United Kingdom":
-    state = ukCountries[secrets.randbelow(len(ukCountries))]
+    state = ukCountries[secrets.randbelow(len(ukCountries))] # picking a random country from the UK countries list
 elif country == "Canada":
-    state = caProvince[secrets.randbelow(len(caProvince))]
+    state = caProvince[secrets.randbelow(len(caProvince))] # picking a random province from the Canadian provinces list
 elif country == "Australia":
-    state = asStates[secrets.randbelow(len(asStates))]
+    state = asStates[secrets.randbelow(len(asStates))] # picking a random state from the Australian states list
 elif country == "New Zealand":
-    state = nzStates[secrets.randbelow(len(nzStates))]
+    state = nzStates[secrets.randbelow(len(nzStates))] # picking a random state from the New Zealand states list
 elif country == "Ireland":
-    state = ieStates[secrets.randbelow(len(ieStates))]
+    state = ieStates[secrets.randbelow(len(ieStates))] # picking a random state from the Irish states list
 else:
-    state = "N/A"
+    state = "N/A" # If the country is not in the predefined lists, set state to "N/A"
 
 #Height and Weight
 if sex == "Male":
-    height = secrets.randbelow(185 - 165 + 1) + 165 #height range for male
+    height = secrets.randbelow(185 - 165 + 1) + 165 #height range for male 
     weight = secrets.randbelow(80 - 60 + 1) + 60 #Weight Range for male
 
 elif sex =="Female":
@@ -145,14 +147,14 @@ personalityTypes = ["ISTJ", "ISFJ","INFJ","INTJ","ISTP","ISFP","INFP","INTP","ES
 personalityType = personalityTypes[secrets.randbelow(len(personalityTypes))] #picking a random personality type
 
 #Output
-print("\n")
-print("Sex: " + sex )
-print(f"Name: {firstName} {lastName}")
-print("Age: " + str(age))  # Output the age
+print("\n") # Print a newline for better readability
+print(f"Sex: " + sex ) # Output the sex of the character
+print(f"Name: {firstName} {lastName}") # Output the full name
+print(f"Age: " + str(age))  # Output the age
 print(f"Birth Date: {month} {day}, {birthYear}")  # Output the birth date 
-print("From: " + state + ", " + country)  # Print the generated location
-print("Height: " + str(height) + "cm" )
-print("Weight: " + str(weight) + "kg" )
-print("Hair Color: " + hairColor)
-print("Eye Color: " + eyeColor)
-print("Personality Type: " + personalityType)
+print(f"From: " + state + ", " + country)  # Print the generated location
+print(f"Height: " + str(height) + "cm" ) # Output the height in centimeters
+print(f"Weight: " + str(weight) + "kg" ) # Output weight in kilograms
+print(f"Hair Color: " + hairColor) # Output the hair color
+print(f"Eye Color: " + eyeColor)  # Output the eye color
+print(f"Personality Type: " + personalityType) # Output the personality type
