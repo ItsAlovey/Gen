e826a0d933f82da78b61da9dea14e341ff9077f2
Alovey <aloveyy@icloud.com>
2025-06-12 13:06:11 -0400

    Update 1.5
    I’ve just removed the comment blocks that were unnecessary. I also change some variable names to be more clear in the purpose they serve.

--- main.py
+++ main.py
@@ -1,104 +1,130 @@
-#Libraries 
+# ===================================================
+# Title: Character Generator
+# Description: This script generates random character 
+#              profiles based on user input.
+# Author: Alovey
+# Date: 2025-06-12
+# Version: 1.5
+# License: MIT License
+# ===================================================
+
+# --- Import Required Modules ---
 import secrets
 import datetime
 
-numOfCharacters = input("How many characters do you want to generate: ") #asking the user how many characters they want to generate
-int(numOfCharacters) #converting the input to an integer
-i = 0 # initializing the counter variable
+# --- Prompt User for Number of Characters ---
+numOfCharacters = input("How many characters do you want to generate: ") 
 
-# Variables for average calculations
-totalMale = 0 # variable to store the total number male characters
-totalFemale = 0 # variable to store the total number of female characters
-totalYears = 0 # variable to store the total years of all characters
-totalAge = 0 # variable to store the total age of all characters
-totalHeight = 0 # variable to store the total height of all characters
-totalWeight = 0 # variable to store the total weight of all characters
+# --- Initialize Counters and Totals ---
+character_count = 0 
+totalMale = 0
+totalFemale = 0
+totalYears = 0
+totalAge = 0
+totalHeight = 0 
+totalWeight = 0 
+youngestAge = 100 
+oldestAge = 0
 
-with open('output.txt', 'w') as output: # opening the output file in append mode
+# --- Open Output File for Writing ---
+with open('output.txt', 'w') as output:
 
-    while i < int(numOfCharacters):
+    # --- Character Generation Loop ---
+    while character_count < int(numOfCharacters):
 
-        #Variables
-        gender = secrets.SystemRandom().randint(0, 1) #pick between male or female name list
-        currentYear = datetime.datetime.now().year  # getting the current year
-        currentMonth = datetime.datetime.now().month  # getting the current month number
-        currentDay = datetime.datetime.now().day  # getting the current day number
-        age = secrets.SystemRandom().randint(13, 80) # generating a random age between 13 and 80
-        birthYear = currentYear - age  # calculating the birth year based on the current year and age
-        monthGen = secrets.SystemRandom().randint(1, 12) # generating a random month number between 1 and 12
+        #Variables for Character Generation
+        gender = secrets.SystemRandom().randint(0, 1) 
 
-        #Gender and Name Generation
+        currentYear = datetime.datetime.now().year  
+        currentMonth = datetime.datetime.now().month  
+        currentDay = datetime.datetime.now().day  
+
+        age = secrets.SystemRandom().randint(13, 80) 
+        birthYear = currentYear - age  
+        birth_month_number = secrets.SystemRandom().randint(1, 12) 
+
+        # --- Determine Gender and Select First Name ---
         if gender == 0: 
             nameList = "male_names.txt"
-            sex = "Male" #setting the sex of the character to male 
+            sex = "Male" #
         elif gender == 1:
             nameList = "female_names.txt"
-            sex = "Female" #setting the sex of the character to female 
-        with open(nameList) as f: #opens file with male names
-            lines = f.readlines() #picks randomly from the list of names
-            firstName = lines[secrets.randbelow(len(lines))].strip() #strips the the newline from the name
-        with open('last_names.txt') as f: #opens file with last names
-            lines = f.readlines() #picks randomly from the list of names
-            lastName = lines[secrets.randbelow(len(lines))].strip()  #strips the the newline from the name
+            sex = "Female" 
+        with open(nameList) as f: 
+            lines = f.readlines() 
+            firstName = lines[secrets.randbelow(len(lines))].strip() 
 
-        #Age and Birth Date
-        if monthGen == 1:
-            month = "January" # setting the month to January
-            days = 31 #setting the number of days in January
-        elif monthGen == 2:
-            month = "February" # setting the month to February
-            # Check if the current year is a leap year to determine the number of days in February
+        # --- Select Last Name ---
+        with open('last_names.txt') as f: 
+            lines = f.readlines() 
+            lastName = lines[secrets.randbelow(len(lines))].strip()  
+
+        # --- Generate Birth Month and Day ---
+        if birth_month_number == 1:
+            month = "January" 
+            days = 31
+
+        elif birth_month_number == 2:
+            month = "February"   
             if birthYear % 4 == 0 and (birthYear % 100 != 0 or birthYear % 400 == 0):
-                days = 29 # If it's a leap year, February has 29 days
+                days = 29 
             else:
-                days = 28  # If it's not a leap year, February has 28 days
-        elif monthGen == 3:
-            month = "March" # setting the month to March
-            days = 31 #setting the number of days in March
-        elif monthGen == 4:
-            month = "April" # setting the month to April
-            days = 30 #setting the number of days in April
-        elif monthGen == 5:
-            month = "May" # setting the month to May
-            days = 31 #setting the number of days in May
-        elif monthGen == 6:
-            month = "June" # setting the month to June
-            days = 30 #setting the number of days in June
-        elif monthGen == 7:
-            month = "July" # setting the month to July
-            days = 31 #setting the number of days in July
-        elif monthGen == 8:
-            month = "August" # setting the month to August
-            days = 31 #setting the number of days in August
-        elif monthGen == 9:
-            month = "September" # setting the month to September
-            days = 30 #setting the number of days in September
-        elif monthGen == 10: 
-            month = "October" # setting the month to October
-            days = 31 #setting the number of days in October
-        elif monthGen == 11:
-            month = "November" # setting the month to November
-            days = 30 #setting the number of days in November
-        elif monthGen == 12:
-            month = "December" # setting the month to December
-            days = 31 #setting the number of days in December
-        else:
-            print("Invalid month number")
-        # This code generates a random month number and prints the corresponding month name.
+                days = 28 
 
-        day = secrets.SystemRandom().randint(1, days)  # generating a random day based on the month
+        elif birth_month_number == 3:
+            month = "March" 
+            days = 31
 
-        if monthGen > currentMonth: #
-            age -= 1  # If the generated month is greater than the current month, subtract one from the age
-        elif monthGen == currentMonth and day > currentDay:
-            age -= 1 # If the generated month is the same as the current month but the day is greater, subtract one from the age
+        elif birth_month_number == 4:
+            month = "April" 
+            days = 30 
 
-        #From
-        with open('countries.txt') as f: #opens file with countries
-            lines = f.readlines() #picking randomly from the list of countries
-        country = lines[secrets.randbelow(len(lines))].strip() # #picking a random country from the list of countries
+        elif birth_month_number == 5:
+            month = "May" 
+            days = 31 
 
-        # States and Provinces 
+        elif birth_month_number == 6:
+            month = "June" 
+            days = 30 
+
+        elif birth_month_number == 7:
+            month = "July" 
+            days = 31 
+
+        elif birth_month_number == 8:
+            month = "August"
+            days = 31 
+
+        elif birth_month_number == 9:
+            month = "September" 
+            days = 30 
+
+        elif birth_month_number == 10: 
+            month = "October"
+            days = 31
+
+        elif birth_month_number == 11:
+            month = "November" 
+            days = 30 
+
+        elif birth_month_number == 12:
+            month = "December" 
+            days = 31 
+
+        # --- Adjust Age Based on Current Date ---
+        birth_day = secrets.SystemRandom().randint(1, days)
+
+        if birth_month_number > currentMonth:
+            age -= 1 
+        elif birth_month_number == currentMonth and birth_day > currentDay:
+            age -= 1 
+
+        # --- Select Country and Corresponding State/Province ---
+        with open('countries.txt') as f:
+            lines = f.readlines()
+        country = lines[secrets.randbelow(len(lines))].strip()
+
+        # --- Define Regional State/Province Lists ---
         usStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
            "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
            "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
@@ -123,88 +149,91 @@ with open('output.txt', 'w') as output: # opening the output file in append mode
               "Monaghan", "Offaly", "Roscommon", "Sligo", "Tipperary", "Waterford", "Westmeath",
               "Wexford", "Wicklow"]
 
+        # --- Determine Region Based on Country Selection ---
         if country == "United States":
-            state = usStates[secrets.randbelow(len(usStates))] # picking a random state from the US states list
+            state = usStates[secrets.randbelow(len(usStates))]
         elif country == "United Kingdom":
-            state = ukCountries[secrets.randbelow(len(ukCountries))] # picking a random country from the UK countries list
+            state = ukCountries[secrets.randbelow(len(ukCountries))] 
         elif country == "Canada":
-            state = caProvince[secrets.randbelow(len(caProvince))] # picking a random province from the Canadian provinces list
+            state = caProvince[secrets.randbelow(len(caProvince))]
         elif country == "Australia":
-            state = asStates[secrets.randbelow(len(asStates))] # picking a random state from the Australian states list
+            state = asStates[secrets.randbelow(len(asStates))] 
         elif country == "New Zealand":
-            state = nzStates[secrets.randbelow(len(nzStates))] # picking a random state from the New Zealand states list
+            state = nzStates[secrets.randbelow(len(nzStates))] 
         elif country == "Ireland":
-            state = ieStates[secrets.randbelow(len(ieStates))] # picking a random state from the Irish states list
+            state = ieStates[secrets.randbelow(len(ieStates))] 
         else:
-            state = "N/A" # If the country is not in the predefined lists, set state to "N/A"
+            state = "N/A" 
 
-    #Height and Weight
+       # --- Generate Height and Weight Based on Gender ---
         if sex == "Male":
-            height = secrets.SystemRandom().randint(165, 185) #height range for male 
-            weight = secrets.SystemRandom().randint(60, 80) #Weight Range for male
+            height = secrets.SystemRandom().randint(165, 185)
+            weight = secrets.SystemRandom().randint(60, 80)
 
         elif sex =="Female":
-            height = secrets.SystemRandom().randint(155, 175) #height range for female
-            weight = secrets.SystemRandom().randint(50, 70) #Weight Range for female
+            height = secrets.SystemRandom().randint(155, 175)
+            weight = secrets.SystemRandom().randint(50, 70)
 
-        #Hair Color
+        # --- Randomly Select Hair and Eye Color ---
         hairColors = [ "Blond", "Dark Blond", "Medium Brown", "Dark Brown", "Black", "Auburn", "Red", "Gray", "White"]
-        hairColor = hairColors[secrets.randbelow(len(hairColors))]  # picking a random hair color
-
-        #Eye Color 
+        hairColor = hairColors[secrets.randbelow(len(hairColors))]
+ 
         eyeColors = ["Brown", "Blue", "Green", "Hazel", "Gray", "Amber"]
-        eyeColor = eyeColors[secrets.randbelow(len(eyeColors))]  # picking a random eye color
+        eyeColor = eyeColors[secrets.randbelow(len(eyeColors))]
 
-        #personality type
+        # --- Randomly Assign Personality Type ---
         personalityTypes = ["ISTJ", "ISFJ","INFJ","INTJ","ISTP","ISFP","INFP","INTP","ESTP","ESFP","ENFP","ENTP","ESTJ","ESFJ","ENFJ","ENTJ"]
-        personalityType = personalityTypes[secrets.randbelow(len(personalityTypes))] #picking a random personality type
+        personalityType = personalityTypes[secrets.randbelow(len(personalityTypes))]
 
-        #Counters for average calculations
+        # --- Update Totals for Statistics ---
         if sex == "Male":
-            totalMale += 1 # incrementing the total number of male characters
+            totalMale += 1 
         elif sex =="Female":
-            totalFemale += 1 # incrementing the total number of female characters
-        else:
-            totalMale == "N/A"
-            totalFemale == "N/A"
+            totalFemale += 1 
 
-        totalYears += birthYear # adding the birth year to the total years
-        totalAge += age # adding the age to the total age
-        totalHeight += height # adding the height to the total height
-        totalWeight += weight # adding the weight to the total weight
+        totalYears += birthYear 
+        totalAge += age 
+        totalHeight += height 
+        totalWeight += weight 
+
+        if age < youngestAge: 
+            youngestAge = age 
+
+        if age > oldestAge:
+            oldestAge = age 
 
 
-        # Writing character profile to the output file
-        output.write(f"\n Character Profile: {i + 1} \n") # writing the character profile number
-        output.write(f"------------------------\n") # writing a separator line
-        output.write(f"Name: {firstName} {lastName}\n") # writing the full name of the character
-        output.write(f"Sex: {sex}\n") # writing]")
-        output.write(f"Age: {age} (Born on {month} {day}, {birthYear})\n") # writing the age and birth date of the character
+        # --- Write Character Profile to Output File ---
+        output.write(f"\n Character Profile: {character_count + 1} \n")
+        output.write(f"------------------------\n") 
+        output.write(f"Name: {firstName} {lastName}\n") 
+        output.write(f"Sex: {sex}\n") 
+        output.write(f"Age: {age} (Born on {month} {birth_day}, {birthYear})\n") 
         output.write(f"From: {state}, {country}\n")
-        output.write(f"Height: {height}cm | Weight: {weight}kg\n") # writing the height and weight of the character
+        output.write(f"Height: {height}cm | Weight: {weight}kg\n") 
         output.write(f"Hair: {hairColor} | Eyes: {eyeColor}\n")
-        output.write(f"Personality Type: {personalityType}\n") # writing the personality type of the character
+        output.write(f"Personality Type: {personalityType}\n") 
         output.write(f"------------------------\n")
 
 
-        if i == int(numOfCharacters): # check if the number of characters generated is equal to the number of characters requested
-            break # exit the loop if the condition is met
-        i += 1 # # increment the counter variable to generate the next character
+        character_count += 1 
+    # --- End of character generation loop ---
 
-    # End of character generation loop
-
-    if numOfCharacters == "1": # check if the user requested only one character
-        output.write(f"\n") # printing a message indicating the character generation is complete
+    # --- Write Summary Statistics if More Than One Character ---
+    if numOfCharacters == "1":
+        output.write(f"\n")
     else:
         output.write(f"\n Character Averages ")
         output.write(f"\n------------------------") 
-        output.write(f"\nTotal Characters: {numOfCharacters}") # printing the total number of characters generated
-        output.write(f"\nMales vs Females: {totalMale} vs {totalFemale}") # printing the total number of females and male characters
-        output.write(f"\nAverage Birth Year: {totalYears / int(numOfCharacters):.0f}") # calculating the average birth year to the nearest whole number
-        output.write(f"\nAverage Age: {totalAge / int(numOfCharacters):.2f} years") # calculating the average age to the second decimal place
-        output.write(f"\nAverage Height: {totalHeight / int(numOfCharacters):.2f}cm") # calculating the average height to the second decimal place
-        output.write(f"\nAverage Weight: {totalWeight / int(numOfCharacters):.2f}kg") # calculating the average weight to the second decimal place
+        output.write(f"\nTotal Characters: {numOfCharacters}") 
+        output.write(f"\nMales vs Females: {totalMale} vs {totalFemale}") 
+        output.write(f"\nAverage Birth Year: {totalYears / int(numOfCharacters):.0f}") 
+        output.write(f"\nAverage Age: {totalAge / int(numOfCharacters):.2f} years") 
+        output.write(f"\nAverage Height: {totalHeight / int(numOfCharacters):.2f}cm") 
+        output.write(f"\nAverage Weight: {totalWeight / int(numOfCharacters):.2f}kg")
+        output.write(f"\nYoungest Age: {youngestAge} years")
+        output.write(f"\nOldest Age: {oldestAge} years")
         output.write("\n------------------------")
 
-print(f"\nCharacter generation complete. Check output.txt for the results.\n") # printing a message indicating the character generation is complete
-# End of the script
+# --- Script Complete ---
+print(f"\nCharacter generation complete. Check output.txt for the results.\n")
